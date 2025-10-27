474179da741bc515ed3130b9e11cde5b8ddad477
Venkata Krishna Surya Satya Gowri Kosuri <satyasatyagowri@gmail.com>
2025-10-27 21:28:01 +0530

    Add files via upload

---
+++ a&p parllelogram.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int b,h;
+	cout<<"enter the base and height:";
+	cin>>b>>h;
+	cout<<"area is:"<<b*h<<endl;
+	cout<<"perimeter is:"<<2*(b*h)<<endl;
+}
