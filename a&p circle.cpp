1ab6a0c6933ad66043383229d993b202174181d9
Venkata Krishna Surya Satya Gowri Kosuri <satyasatyagowri@gmail.com>
2025-10-27 21:26:12 +0530

    Add files via upload

---
+++ a&p circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the radius of a circle:";
+	cin>>a;
+	cout<<"area is:"<<(3.14)*a*a<<endl;
+	cout<<"perimeter is:"<<2*(3.14)*a<<endl;
+}
+
