d94df049746319d8e9cb0d583a54ebb54139bc59
Venkata Krishna Surya Satya Gowri Kosuri <satyasatyagowri@gmail.com>
2025-10-28 10:30:07 +0530

    Add files via upload

---
+++ a&p rhombus.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a1,a2,d;
+	cout<<"enter the diagnols &length of a side:";
+	cin>>a1>>a2>>d;
+	cout<<"area is:"<<(a1*a2)/2<<endl;
+	cout<<"perimeter is:"<<4*d<<endl;
+}
