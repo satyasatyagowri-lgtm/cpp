51f3bdc2a14155545232b39f8ed6c1e06b089f9a
Venkata Krishna Surya Satya Gowri Kosuri <satyasatyagowri@gmail.com>
2025-10-29 20:30:45 +0530

    Add files via upload

---
+++ a&p square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the length of a side:";
+	cin>>a;
+	cout<<"area is:"<<a*a<<endl;
+	cout<<"perimeter is:"<<4*a<<endl;
+}
